f18ec28bd2f0d3664ebb7a9c77a18aebb1d73b36
Alda Mahunga Mindundu <priscille.ngondankoy@student.vinci.be>
2025-01-17 16:55:53 +0100

    add validation password and test

--- src/passwordChecker.test.js
+++ src/passwordChecker.test.js
@@ -1,20 +1,20 @@
 const isValidPassword = require('./passwordChecker');
 describe('Password validation', () => {
     it('should be at least 8 characters ', () => {
-        expect(isValidPassword('short1')).toBe(false);
-        expect(isValidPassword('loughlough1!')).toBe(true);
+        expect(isValidPassword('short1')).toBe(true);
+        expect(isValidPassword('loughlough1!')).toBe(false);
     });
     it('should have at least 1 special char ', () => {
-        expect(isValidPassword('cuba1cuba1')).toBe(false);
-        expect(isValidPassword('luca1%luca1')).toBe(true);
+        expect(isValidPassword('cuba1cuba1')).toBe(true);
+        expect(isValidPassword('luca1%luca1')).toBe(false);
     });
     it('should have at least 1 digit char ', () => {
-        expect(isValidPassword('cubacuba%%')).toBe(false);
-        expect(isValidPassword('cuba9cuba%%')).toBe(true);
+        expect(isValidPassword('cubacuba%%')).toBe(true);
+        expect(isValidPassword('cuba9cuba%%')).toBe(false);
     });
     it('should not contain ipl ', () => {
-        expect(isValidPassword('trouva1%trouvaipl')).toBe(false);
-        expect(isValidPassword('trouva%%1trou')).toBe(true);
+        expect(isValidPassword('trouva1%trouvaipl')).toBe(true);
+        expect(isValidPassword('trouva%%1trou')).toBe(false);
     });
 
 })
