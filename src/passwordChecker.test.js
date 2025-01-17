71011b3cc50e233016c4d00cbc20b414b05d83ab
Alda Mahunga Mindundu <priscille.ngondankoy@student.vinci.be>
2025-01-17 17:02:37 +0100

    add validation password and test

--- src/passwordChecker.test.js
+++ src/passwordChecker.test.js
@@ -1,15 +1,15 @@
 const isValidPassword = require('./passwordChecker');
 describe('Password validation', () => {
     it('should be at least 8 characters ', () => {
-        expect(isValidPassword('short1')).toBe(true);
+        expect(isValidPassword('short1')).toBe(false);
         expect(isValidPassword('loughlough1!')).toBe(false);
     });
     it('should have at least 1 special char ', () => {
-        expect(isValidPassword('cuba1cuba1')).toBe(true);
+        expect(isValidPassword('cuba1cuba1')).toBe(false);
         expect(isValidPassword('luca1%luca1')).toBe(false);
     });
     it('should have at least 1 digit char ', () => {
-        expect(isValidPassword('cubacuba%%')).toBe(true);
+        expect(isValidPassword('cubacuba%%')).toBe(false);
         expect(isValidPassword('cuba9cuba%%')).toBe(false);
     });
     it('should not contain ipl ', () => {
